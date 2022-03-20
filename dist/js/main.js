fa7013a376d5019a67bfebd48d7baac3e7c6a707
Corey Snyder <corey_snyder33@yahoo.com>
2022-03-20 09:22:43 -0400

    Menu button, javascript & rotate

---
+++ dist/js/main.js
@@ -0,0 +1,34 @@
+// SELECT DOM ITEMS
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItem = document.querySelectorAll('.nav-items');
+
+// SET THE INITIAL STATE OF THE MENU
+let showMenu = false;
+
+// EVENT LISTENER
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItem.forEach((item) => item.classlist.add('show'));
+
+    // SET MENU STATE
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItem.forEach((item) => item.classlist.remove('show'));
+
+    // SET MENU STATE
+    showMenu = false;
+  }
+}
