d5172d2a6e9b1e5e22e61fdb66bce96aba5aa64c
Corey Snyder <corey_snyder33@yahoo.com>
2022-03-25 21:34:08 -0400

    Menu button. javascript & rotate

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,13 @@
-// SELECT DOM ITEMS
+// Select DOM Items
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItem = document.querySelectorAll('.nav-items');
+const navItems = document.querySelectorAll('.nav-item');
 
-// SET THE INITIAL STATE OF THE MENU
+// Set Initial State Of Menu
 let showMenu = false;
 
-// EVENT LISTENER
 menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
@@ -17,18 +16,18 @@ function toggleMenu() {
     menu.classList.add('show');
     menuNav.classList.add('show');
     menuBranding.classList.add('show');
-    navItem.forEach((item) => item.classlist.add('show'));
+    navItems.forEach((item) => item.classList.add('show'));
 
-    // SET MENU STATE
+    // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove('close');
     menu.classList.remove('show');
     menuNav.classList.remove('show');
     menuBranding.classList.remove('show');
-    navItem.forEach((item) => item.classlist.remove('show'));
+    navItems.forEach((item) => item.classList.remove('show'));
 
-    // SET MENU STATE
+    // Set Menu State
     showMenu = false;
   }
 }
